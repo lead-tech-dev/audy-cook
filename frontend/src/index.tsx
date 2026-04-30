0b50c31cb6cfbf86b3f6d81dad19dff9080b6337
Eric Maximan <ericmaximan@gmail.com>
2026-04-30 12:39:03 +0200

    Make router mode configurable for SPA hosting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,17 @@
 // AUDY COOK - Frontend entry
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "@/App";
 import "@/styles/global.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const Router = process.env.REACT_APP_ROUTER_MODE === "hash" ? HashRouter : BrowserRouter;
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
